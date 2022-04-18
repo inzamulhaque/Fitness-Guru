b00d56636f86fd8dae2806c78678eb8e4483bbcf
inzamulhaque <mdihalif@gmail.com>
2022-04-18 16:14:29 +0600

    add env file

--- src/firebase.init.js
+++ src/firebase.init.js
@@ -6,12 +6,18 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyBKCHqwJjb0KQ71rC09x7uksyCRr9KYjLo",
-    authDomain: "fitness-guru-assignment-10.firebaseapp.com",
-    projectId: "fitness-guru-assignment-10",
-    storageBucket: "fitness-guru-assignment-10.appspot.com",
-    messagingSenderId: "1035866170873",
-    appId: "1:1035866170873:web:d50de19864676ea9648928"
+    // apiKey: "AIzaSyBKCHqwJjb0KQ71rC09x7uksyCRr9KYjLo",
+    // authDomain: "fitness-guru-assignment-10.firebaseapp.com",
+    // projectId: "fitness-guru-assignment-10",
+    // storageBucket: "fitness-guru-assignment-10.appspot.com",
+    // messagingSenderId: "1035866170873",
+    // appId: "1:1035866170873:web:d50de19864676ea9648928"
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTHDOMAIN,
+    projectId: process.env.REACT_APP_PROJECTID,
+    storageBucket: process.env.REACT_APP_SBUCKET,
+    messagingSenderId: process.env.REACT_APP_MSID,
+    appId: process.env.REACT_APP_APPID
 };
 
 // Initialize Firebase
