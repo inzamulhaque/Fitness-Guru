4772443af21f2ee9555515a0f3ab73d0c287fd7b
inzamulhaque <mdihalif@gmail.com>
2022-04-18 15:42:52 +0600

    create contact page

--- src/components/Home/ContactForm.js
+++ src/components/Home/ContactForm.js
@@ -1,11 +1,29 @@
 import React from 'react';
+import { ToastContainer, toast } from 'react-toastify';
 
 const ContactForm = () => {
+
+    // handle Contact function
+    const handleContact = event => {
+        event.preventDefault();
+        const fname = event.target.fname.value;
+        const lname = event.target.lname.value;
+
+        toast.success(`${fname} ${lname} thanks for message`, {
+            position: "top-center",
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+        });
+    }
     return (
         <div className="w-full md:w-3/4 lg:w-1/2 mx-auto mt-3">
             <div className="card p-3">
                 <h3 className="text-[25px] m-2 text-center text-blue-500 font-bold">Contact Us</h3>
-                <form>
+                <form onSubmit={handleContact}>
                     <div className="md:flex gap-2 justify-center items-center">
                         <input type="text" placeholder='Enter Your First Name*' className='w-full md:w-1/2 border-b-2 border-black focus:outline-none py-2 px-5 text-[22px]' name="fname" required />
                         <input type="text" placeholder='Enter Your Last Name*' className='w-full md:w-1/2 border-b-2 border-black focus:outline-none py-2 px-5 text-[22px]' name="lname" required />
@@ -19,8 +37,9 @@ const ContactForm = () => {
 
                     <button type="submit" className='py-2 px-7 bg-blue-500 text-white font-bold text-[22px] border-2 border-blue-500 hover:bg-transparent hover:text-blue-500 duration-500 ease-in-out rounded-md mt-3 block md:inline-block mx-auto mb-2'>Send</button>
                 </form>
+                <ToastContainer />
             </div>
-        </div>
+        </div >
     );
 };
 
